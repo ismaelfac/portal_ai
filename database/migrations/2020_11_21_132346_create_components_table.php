56761b195478bb8a8ce55909608522311d757ec6
Ismael Lastre Alvarez <ismaelfac@gmail.com>
2020-11-21 08:43:22 -0500

    Base Project

---
+++ database/migrations/2020_11_21_132346_create_components_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComponentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('components', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->mediumText('description');
+            $table->boolean('isActive')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('components', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+    }
+}
